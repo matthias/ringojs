66747f96fa205082cf3221ff24d144cd1cdba569
Matthias <matthias.platzer@knallgrau.at>
2008-08-28 14:55:34 +0200

    Implemented mixin functionality for Controllers, using robis inheritance module

--- modules/aida/controller/controller.js
+++ modules/aida/controller/controller.js
@@ -8,7 +8,8 @@ logging.setConfig(getResource('config/environments/development/log4j.properties'
 (function () {
    
    var logger = logging.getLogger(__name__);
-   
+
+/*   
    this.getControllerInstance = function(name) {
       try {
          importModule("app.controllers." + name + "_controller");      
@@ -26,6 +27,45 @@ logging.setConfig(getResource('config/environments/development/log4j.properties'
       c.importHelpers(name)
       return c;
    }
+   */
+   
+   this.getController = function(name) {
+      if (typeof name === "string") {
+         var className = (name.match(/^(.*)Controller$/)) ? name : getClassNameFromName(name);
+         constructor = importModule("app.controllers." + name + "_controller")[className];
+         if (!constructor) {
+            throw new Error("Couldn't find controller " + getClassNameFromName(constructor) + " in app/controllers/" + constructor + "_controller");
+         }         
+      }
+      // var proto = Object.clone(constructor.prototype);
+      constructor.prototype = importModule("aida.controller");
+      constructor.prototype.__name__ = className;
+      // Object.extend(constructor.prototype, proto);
+      return constructor;
+
+//      c.importHelpers("application")
+//      c.importHelpers(name)
+   }
+
+   /**
+    * 
+    */
+   this.getControllerInstance = function(name) {
+      var constructor = this.getController(name);
+      return new constructor();
+   }   
+   
+   /**
+    * Inherit from another controller.
+    *
+    */
+   this.mixin = function(constructor) {
+      var mixin = importModule("helma.inheritance").mixin;
+      if (typeof constructor === "string") {
+         constructor = this.getController(constructor);
+      }
+      mixin(this, constructor);
+   }   
    
    this.actions = {};
 
